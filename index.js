a6f67a71415bfee7d967c085ef63f9f4d4c84549
mohammedsbd <Mohammed1112ok@gmail.com>
2025-04-02 09:25:52 -0700

    setup express app

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express'); //requiring express
+import express from 'express'; //requiring express
 const app = express(); //invoking the express
 const PORT=3000;
 
 
 // define the route
 app.get('/',(req, res) =>{
-    res.send('Hello, express');
+    res.send('Hello, world');
 })
 
 app.listen(PORT,()=>{
