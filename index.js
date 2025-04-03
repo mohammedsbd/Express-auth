98533fead76df56b85dafabaffa1e11ee5ca62d4
mohammedsbd <johndoe@example.com>
2025-04-03 10:42:28 -0700

    query route

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 import express from 'express'; //requiring express
 const app = express(); //invoking the express
-const PORT=3000;
+const PORT = 3000;
 
 
 // define the route
 app.get('/',(req, res) =>{
     res.send('Hello, world');
 })
+
+// dynamic routing
+app.get('/user/:username',(req, res) =>{
+    const username = req.params.username;
+    res.send(`welcome ${username}`)
+})
+
+
+app.get('search/',(req,res)=>{
+    const keyword=req.query.keyword;
+    res.send(`searching for ${keyword}`)
+})
+
 
 app.listen(PORT,()=>{
     console.log(`server is running at http://localhost:${PORT}`);
